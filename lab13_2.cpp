05e240c9560775a44fe3b0e58864a281ccad7da5
Mosjitos <kamolrat_saeboon@cmu.ac.th>
2024-01-02 16:59:38 +0700

    lab13_2.cpp completed ;]

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -21,3 +21,27 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+
+void showImage(const bool image[N][M]){
+    cout << "------------------------------------------------------------------------";
+    for(int j=0; j<30; j++){
+    cout << "\n|";
+    for(int i=0; i<70; i++){
+        if(image[j][i] == 1) cout << "*";
+        else cout << " ";
+    }
+    cout << "|";
+    }
+    cout << "\n------------------------------------------------------------------------\n";
+}
+
+void updateImage(bool image[][M],int s,int x,int y){
+    
+    for(int j=0; j<N; j++){
+    for(int i=0; i<M; i++){
+        if(sqrt(pow((j-x),2)+(pow((i-y),2)))<=s-1) image[j][i]=1;
+        
+    }
+    }
+}
